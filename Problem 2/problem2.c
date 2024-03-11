1c7a5f5ba96c170830c85645f46db065fc6d44c4
aidenhburke <aidenburke@ku.edu>
2024-03-10 19:26:39 -0500

    Create problem2.c

---
+++ Problem 2/problem2.c
@@ -0,0 +1,12 @@
+/*
+Program: EECS 348 Lab 05 Problem 2
+Function: Create a program that will take an input football score and return all possible ways to achieve that score
+Author: Aiden Burke
+Date Modified: 03/10/2024
+*/
+#include <stdio.h>
+int main(){
+
+
+    return 0;
+}
